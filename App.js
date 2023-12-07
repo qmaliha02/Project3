23388158f36a2aad2d578eb8f2d551b62a45c698
qmaliha02 <144977322+qmaliha02@users.noreply.github.com>
2023-12-06 19:16:47 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,82 +1,130 @@
+
 import './App.css';
 import { useState, useEffect } from "react";
 import Axios from "axios";
 
-
-
-
 function App() {
-
-  const[listofProb, setlistofProb] = useState ([]);
-  const[reqid, setID]= useState(0);
-  const[aptnum, setNum]= useState(0);
-  const[probarea, setArea]= useState("");    
-  const[description, setDesc]= useState("");   
-  const[photo, setPhoto]= useState("");   
-  const[status, setStat]= useState("");   
-  const[date, setDate]= useState(0); 
-
-  const createRequest =() => {
-
-    Axios.post("http://localhost:3001/createRequest",{
-  reqid,
-  aptnum,
-  probarea,
-  description,
-  photo,
-  status,
-  date
-  
-    }).then((response) => {
-      setlistofProb([...listofProb, {reqid,
-        aptnum,
-        probarea,
-        description,
-        photo,
-        status,
-        date
-  
-      },]);
-    });
-  
-  };
+  const [ListofTent, setListofTent] = useState([]);
+  const[ID, setID]= useState(0);
+  const[Name, setName] = useState("");
+  const[Phone_Number, setPNumber] = useState(0);
+  const[Email, setEmail] = useState("");
+  const[Date_of_CheckIn, setCheckIn] = useState("");
+  const[Date_of_CheckOut, setCheckOut] = useState("");
+  const[Apt_Num, setApt] = useState(0);
 
 
+useEffect (() => 
+{
+  Axios.get("http://localhost:3001/getTenant").then((response) => {
+    setListofTent(response.data);
+  });
+},[]);
 
 
+const CreateTenant =() => {
+
+  Axios.post("http://localhost:3001/createTenant",{
+ID,
+Name,
+Phone_Number,
+Email,
+Date_of_CheckIn,
+Date_of_CheckOut,
+Apt_Num
+
+  }).then((response) => {
+    setListofTent([...ListofTent, {ID,
+      Name,
+      Phone_Number,
+      Email,
+      Date_of_CheckIn,
+      Date_of_CheckOut,
+      Apt_Num
+
+    },]);
+  });
+
+};
+
+const DeleteItem = (id, Name) => {
+  if(window.confirm(`Are you sure you want to delete ${Name}`)){
+
+fetch("http://localhost:3001/deleteTenant", {
+method: "POST",
+crossDomain: true,
+headers: {
+  "Content-type": "application/json",
+  Accept: "application/json",
+  "Access-Control-Allow-Origin": "*",
+},
+body: JSON.stringify({
+  id : id,
+}),
+})
+.then((res) => res.json())
+.then((data) => {
+alert(data.data);
+})
+  }else{
+
+  }
+};
 
   return (
     <div className="App">
       <div className="Title">
-        <h1>Maintenance Request</h1>
-        <h1>Please fill this form and our staff will reach out as soon as possible.</h1>
+        <h1>Manager</h1>
+        <h1>List of Tenants</h1>
       </div>
-      <div className="Request">
-             <input type="number" placeholder="Request ID..."onChange={(event) => {
+      <div className="TentDisplay">
+        {
+          ListofTent.map((user)=>{
+            return(
+           <div className="ItemContainer">
+            <div className="Display">
+              <h1>ID: {user.ID}</h1>
+              <h1>Name: {user.Name}</h1>
+              <h1>Phone Number: {user.Phone_Number}</h1>
+              <h1>Email: {user.Email}</h1>
+              <h1>Date of Check-in: {user.Date_of_CheckIn}</h1>
+              <h1>Date of Check-out: {user.Date_of_CheckOut}</h1>
+              <h1>Apartment Number: {user.Apt_Num}</h1>
+              </div>
+              <button onClick={() =>{
+               DeleteItem(user._id, user.Name);
+            }}>Delete</button>
+              </div>
+            ); 
+        })}
+
+      </div>
+      <div className="Create">
+        <h1>Create New Tenant</h1>
+        <input type="number" placeholder="ID..."onChange={(event) => {
           setID(event.target.value);
         }}/>
+        <input type="text" placeholder="Name..."onChange={(event) => {
+          setName(event.target.value);
+        }}/>
+        <input type="number" placeholder="Phone Number..."onChange={(event) => {
+          setPNumber(event.target.value);
+        }}/>
+        <input type="text" placeholder="Email..."onChange={(event) => {
+          setEmail(event.target.value);
+        }}/>
+        <input type="text" placeholder="date of check in..."onChange={(event) => {
+          setCheckIn(event.target.value);
+        }}/>
+        <input type="text" placeholder="date of check out..."onChange={(event) => {
+          setCheckOut(event.target.value);
+        }}/>
         <input type="number" placeholder="Apartment Number..."onChange={(event) => {
-          setNum(event.target.value);
+          setApt(event.target.value);
         }}/>
-        <input type="text" placeholder="Problem Area..."onChange={(event) => {
-          setArea(event.target.value);
-        }}/>
-        <div className="Special">
-        <input type="text" placeholder="Description..."onChange={(event) => {
-          setDesc(event.target.value);
-        }}/>
-        </div>
-        <input type="text" placeholder="Photo..."onChange={(event) => {
-          setPhoto(event.target.value);
-        }}/>
-        <input type="text" placeholder="Status..."onChange={(event) => {
-          setStat(event.target.value);
-        }}/>
-        <input type="text" placeholder="Date..."onChange={(event) => {
-          setDate(event.target.value);
-        }}/>
-        <button onClick={createRequest}> Create Request</button>
-        </div>
+        <button onClick={CreateTenant}> Create Tenant</button>
+      </div>
+      
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const  express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-const ProblemModel = require('./models/Problem');
+const TenantModel = require('./models/Tenants');
 const cors = require("cors");
 
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://qlm5011:Password123@cluster10.jmhzyjc.mongodb.net/apt?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://qlm5011:Password123@cluster1.kyrrnfm.mongodb.net/files?retryWrites=true&w=majority");
 
-app.post("/createRequest",async (req,res) => {
+app.get("/getTenant",async (req,res) => {
+    try {
+        const result = await TenantModel.find({});
+        res.json(result);
+    } catch (err) {
+        res.json(err);
+    }
+});
+app.post("/createTenant",async (req,res) => {
     const item =  req.body;
-    const newItem = new ProblemModel (item);
+    const newItem = new TenantModel (item);
     await newItem.save();
 
     res.json(item);
-
+    });
     
+app.post("/deleteTenant", async (req,res) => {
+        const {id }= req.body;
+        try{
+            itemsModel.deleteOne({_id: id}, function(err, res) {
+                console.log(err);
+            });
+        } catch(error) {
+            console.log(error);
+            
+        }
+        
+    
+       
     });
 
 
 
 app.listen(3001, () =>{
-    console.log("server runs!");
-    });
+console.log("server runs!");
+});
